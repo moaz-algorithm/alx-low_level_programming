379f20dac1eb96ca1897b5b797ebb76d05208ad6
moaz-algorithm <mrmaz981@gmail.com>
2023-08-11 17:58:50 -0700

    upload script files at my dic

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Entry point
  *
- * Description: A C program that prints with put function.
+ * * main - Entry point
  *
- * Return: Alawys 0 (Success)
-*/
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
-}
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
 
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
