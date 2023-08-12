d22b0249a70a9e163ea8aca085cfee7c532692fa
moaz-algorithm <mrmaz981@gmail.com>
2023-08-11 18:04:06 -0700

    upload script files at my dic

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
-* main - Entry point
-*
-* Description: A C program that prints with printf function.
-*
-* Return: Alawys 0 (Success)
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
 */
+
 int main(void)
+
 {
-	printf('with proper grammar, but the outcome is a piece of art,\n');
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
 }
+
+
