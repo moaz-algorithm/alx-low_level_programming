9482274077f7214122a2f9d2878d210d0d4de226
moaz-algorithm <mrmaz981@gmail.com>
2023-08-14 16:37:51 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 nclude < stdio.h >
 
 /**
- * main - Prints the alphabet.
- *
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char ch;
 
-	for (i = 0; i < 26; i++)
-{
-		putchar(alp[i]);
-}
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
