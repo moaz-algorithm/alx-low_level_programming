65c7e23464b332484c153d404682b96cac2b6d35
moaz-algorithm <mrmaz981@gmail.com>
2023-08-16 13:56:58 -0700

    upload script files at my dic

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,16 @@
-#include "main.h"
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-
- * main - print putchar
-
- * Return: always 0
-
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 
-
 int main(void)
-
 {
 
-        _putchar('_');
+printf("_putchar\n");
 
-        _putchar('p');
-
-        _putchar('u');
-
-        _putchar('t');
-
-        _putchar('c');
-
-        _putchar('h');
-
-        _putchar('a');
-
-        _putchar('r');
-
-        _putchar('\n');
-
-
-        return (0);
+return (0);
 }
